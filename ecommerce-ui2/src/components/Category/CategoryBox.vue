45f6ac01e7c906a6f3023a1295959afe5f2715ce
dilipkumarcruze <dilip9566083688@gmail.com>
2023-03-08 22:42:39 +0530

    ui

---
+++ ecommerce-ui2/src/components/Category/CategoryBox.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card w-100 h-100" style="margin-top: 10px">
+        <div >
+            <img
+                class="card-img-top" :src="category.imageUrl"
+                alt="Card image cap"
+            />
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">{{ category.categoryName }}</h5>
+            <p class="card-text">{{ category.description }}</p>
+            <a href="#" class="btn btn-primary">Go Somewhere</a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "CategoryBox",
+    props: ["category"],
+    methods: {},
+};
+</script>
---
+++ ecommerce-ui2/src/views/Category/AddCategory.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-11 text-center">
+                <h3 class="pt-3">Add Category</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-5">
+                <form>
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" class="form-control" v-model="categoryName" />
+                </div>
+                <div class="form-group">
+                    <label>Description</label>
+                    <textarea type="text" class="form-control" v-model="description"/>
+                </div>
+                <div class="form-group">
+                    <label>Image</label>
+                    <input type="text" class="form-control" v-model="imageUrl"/>
+                </div>
+                <button type="button" class="btn btn-success" @click="addCategory">Submit</button>
+            </form>
+            </div>
+            <div class="col-3"></div>
+            
+        </div>
+    </div>
+</template>
+<script>
+const axios=require("axios");
+const sweetalert = require("sweetalert");
+
+export default {
+    data() {
+        return{
+            categoryName:"",
+            description:"",
+            imageUrl:"",
+        };
+    },
+    methods:{
+        addCategory(){
+            console.log(this.categoryName, this.description,this.imageUrl);
+            const newCategory ={
+                categoryName:this.categoryName,
+                description:this.description,
+                imageUrl:this.imageUrl,
+            };
+
+            const baseURL = "http://localhost:8090";
+            axios({
+                method:'post',
+                url:`${baseURL}/category/create`,
+                data:JSON.stringify(newCategory),
+                headers:{
+                    'content-type':'application/json'
+                }
+            }).then(()=>{
+                sweetalert({
+                    text:"Category Added Successfully",
+                    icon:"success",
+                });
+            }).catch((err)=>{
+                
+                console.log(err);
+            });
+        },
+        
+    },
+};
+</script>
+<style scoped></style>
