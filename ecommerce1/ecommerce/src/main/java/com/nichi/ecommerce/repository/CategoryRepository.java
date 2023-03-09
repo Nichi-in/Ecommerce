f6646a0e243855163476fa44d1f052746c91f2e0
Naya-99 <108174608+Naya-99@users.noreply.github.com>
2023-03-09 12:17:54 +0530

    Delete ecommerce1/ecommerce directory

--- ecommerce1/ecommerce/src/main/java/com/nichi/ecommerce/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nichi.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nichi.ecommerce.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer>{
-
-}
